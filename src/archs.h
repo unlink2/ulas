2df91ddef7a750f07b8b4b86deb10886351b945c
Lukas Krickl <lukas@krickl.dev>
2024-02-19 17:39:40 +0100

    Fixed naming for sm83

--- src/archs.h
+++ src/archs.h
@@ -1,39 +1,38 @@
 #ifndef ARCHS_H_
 #define ARCHS_H_
 
-// TODO: rename reg constants so that they contain SM83
 enum ulas_asmregs_sm83 {
   // r8
-  ULAS_REG_B = 1,
-  ULAS_REG_C = 2,
-  ULAS_REG_D = 3,
-  ULAS_REG_E = 4,
-  ULAS_REG_H = 5,
-  ULAS_REG_L = 6,
-  ULAS_REG_A = 7,
+  ULAS_REGSM83_B = 1,
+  ULAS_REGSM83_C = 2,
+  ULAS_REGSM83_D = 3,
+  ULAS_REGSM83_E = 4,
+  ULAS_REGSM83_H = 5,
+  ULAS_REGSM83_L = 6,
+  ULAS_REGSM83_A = 7,
 
   // r16
-  ULAS_REG_BC = 8,
-  ULAS_REG_DE = 9,
-  ULAS_REG_HL = 10,
-  ULAS_REG_AF = 16,
+  ULAS_REGSM83_BC = 8,
+  ULAS_REGSM83_DE = 9,
+  ULAS_REGSM83_HL = 10,
+  ULAS_REGSM83_AF = 16,
 
   // flags
-  ULAS_REG_NOT_ZERO = 11,
-  ULAS_REG_ZERO = 12,
-  ULAS_REG_NOT_CARRY = 13,
-  ULAS_REG_CARRY = 14,
+  ULAS_REGSM83_NOT_ZERO = 11,
+  ULAS_REGSM83_ZERO = 12,
+  ULAS_REGSM83_NOT_CARRY = 13,
+  ULAS_REGSM83_CARRY = 14,
 
   // misc
-  ULAS_REG_SP = 15,
-  ULAS_VEC00 = 17,
-  ULAS_VEC08 = 18,
-  ULAS_VEC10 = 19,
-  ULAS_VEC18 = 20,
-  ULAS_VEC20 = 21,
-  ULAS_VEC28 = 22,
-  ULAS_VEC30 = 23,
-  ULAS_VEC38 = 24,
+  ULAS_REGSM83_SP = 15,
+  ULAS_VECSM83_00 = 17,
+  ULAS_VECSM83_08 = 18,
+  ULAS_VECSM83_10 = 19,
+  ULAS_VECSM83_18 = 20,
+  ULAS_VECSM83_20 = 21,
+  ULAS_VECSM83_28 = 22,
+  ULAS_VECSM83_30 = 23,
+  ULAS_VECSM83_38 = 24,
 
   ULAS_SM83_REGS_LEN
 };
@@ -43,7 +42,7 @@ enum ulas_archs { ULAS_ARCH_SM83 };
 struct ulas_arch {
   const char **regs_names;
   unsigned long regs_len;
-  
+
   const struct ulas_instr *instrs;
 };
 
